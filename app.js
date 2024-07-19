67cc4bf982f7aeb96e85b285fc10b6d101b37fa5
omiras <oscar.miras@gmail.com>
2024-07-19 13:11:27 +0200

    commented limiter for now

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true })); // Middleware para parsear datos de formularios
 
 const limiter = rateLimit({
-    windowMs: 24 * 60 * 60 * 1000, 
-    max: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  windowMs: 24 * 60 * 60 * 1000,
+  max: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
-app.use(limiter);
+// app.use(limiter);
 
 // obtener el index.html
 app.get("/", (req, res) => {
@@ -37,41 +37,41 @@ app.get("/", (req, res) => {
 
 // obtener una pregunta tipo test aleatoria
 app.get("/api/v1/question/random", (req, res) => {
-    // TODO!
-    const { category } = req.query; 
-    let multipleCategoryQuestions = []
-    console.log(category)
-    const question = () => {
-      if (category) {
-         if (category.includes(',')) {
-            const categoryArr = category.split(',');
-             multipleCategoryQuestions = questions.filter(q => {
+  // TODO!
+  const { category } = req.query;
+  let multipleCategoryQuestions = []
+  console.log(category)
+  const question = () => {
+    if (category) {
+      if (category.includes(',')) {
+        const categoryArr = category.split(',');
+        multipleCategoryQuestions = questions.filter(q => {
 
-                for (i=0; i<categoryArr.length; i++) {
-                  if ( q.category == categoryArr[i]) {
-                    return true;
-                  }
-                }
-            })
-            console.log(multipleCategoryQuestions)
-            const randomMultipleCategoryQuestion  = _.sample(multipleCategoryQuestions);
-            return randomMultipleCategoryQuestion;
-         } else {
-          const categoryQuestions = questions.filter(
-                (q) => q.category.toLowerCase() == category.toLowerCase()
-              );
-              const randomCategoryQuestion = _.sample(categoryQuestions); 
-              return randomCategoryQuestion;
-         }
+          for (i = 0; i < categoryArr.length; i++) {
+            if (q.category == categoryArr[i]) {
+              return true;
+            }
+          }
+        })
+        console.log(multipleCategoryQuestions)
+        const randomMultipleCategoryQuestion = _.sample(multipleCategoryQuestions);
+        return randomMultipleCategoryQuestion;
+      } else {
+        const categoryQuestions = questions.filter(
+          (q) => q.category.toLowerCase() == category.toLowerCase()
+        );
+        const randomCategoryQuestion = _.sample(categoryQuestions);
+        return randomCategoryQuestion;
+      }
 
-        } else {
-          const randomQuestion = _.sample(questions); //De Lodash
-          return randomQuestion;
-        }
-    } 
-  
-    res.send(question());
-  });
+    } else {
+      const randomQuestion = _.sample(questions); //De Lodash
+      return randomQuestion;
+    }
+  }
+
+  res.send(question());
+});
 
 
 // obtener todas las categorías posibles ordenadas alfabeticamente
@@ -82,42 +82,42 @@ app.get("/api/v1/categories", (req, res) => {
   res.send(orderedArr);
 });
 
- 
+
 // Endpoint upvote
 app.patch('/api/v1/question/:id/upvote', limiter, (req, res) => {
-    const id = req.params.id;
-    const question = questions.find(q => q.id === id);
+  const id = req.params.id;
+  const question = questions.find(q => q.id === id);
 
-    if (question) {
-        if (!question.hasOwnProperty('popularity')) {
-            question.popularity = 0;
-        }
-        question.popularity += 1;
-        // Guardar los cambios en el archivo questions.json
-        fs.writeFileSync('./questions.json', JSON.stringify(questions, null, 2));
-        res.status(200).send({ message: 'Upvoted successfully', question });
-    } else {
-        res.status(404).send({ message: 'Question not found' });
+  if (question) {
+    if (!question.hasOwnProperty('popularity')) {
+      question.popularity = 0;
     }
+    question.popularity += 1;
+    // Guardar los cambios en el archivo questions.json
+    fs.writeFileSync('./questions.json', JSON.stringify(questions, null, 2));
+    res.status(200).send({ message: 'Upvoted successfully', question });
+  } else {
+    res.status(404).send({ message: 'Question not found' });
+  }
 });
 
 // Endpoint para cuando el usuario vote negativo
-app.patch('/api/v1/question/:id/downvote', limiter, (req,res) => {
-    const id = req.params.id;
-    const question = questions.find(q => q.id === id);
+app.patch('/api/v1/question/:id/downvote', limiter, (req, res) => {
+  const id = req.params.id;
+  const question = questions.find(q => q.id === id);
 
-    if (question) {
-        if (!question.hasOwnProperty('popularity')) {
-            question.popularity = 0;
-        }
-        question.popularity -= 1;
-        // Guardar los cambios en el archivo questions.json
-        fs.writeFileSync('./questions.json', JSON.stringify(questions, null, 2));
-        res.status(200).send({ message: 'Downvoted successfully', question });
-    } else {
-        res.status(404).send({ message: 'Question not found' });
+  if (question) {
+    if (!question.hasOwnProperty('popularity')) {
+      question.popularity = 0;
     }
-} )
+    question.popularity -= 1;
+    // Guardar los cambios en el archivo questions.json
+    fs.writeFileSync('./questions.json', JSON.stringify(questions, null, 2));
+    res.status(200).send({ message: 'Downvoted successfully', question });
+  } else {
+    res.status(404).send({ message: 'Question not found' });
+  }
+})
 
 // Levantar el servidor
 app.listen(process.env.PORT || 3000, () => {
